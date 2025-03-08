3039b7675591ea1938feca11808d3854b84e15d0
Kevnel YT <kevichen510@gmail.com>
2025-03-07 22:53:51 -0500

    Changes

--- BasicTactics/InitiationLevel.cs
+++ BasicTactics/InitiationLevel.cs
@@ -12,8 +12,12 @@ namespace BasicTactics
 {
     public partial class InitiationLevel : UserControl
     {
-        List<BlockGen> map = new List<BlockGen>();
+        List<BlockGen> Bridgemap = new List<BlockGen>();
+        List<BlockGen> Walkablemap = new List<BlockGen>();
+        List<BlockGen> Seamap = new List<BlockGen>();
         SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
 
         public InitiationLevel()
         {
@@ -24,36 +28,94 @@ namespace BasicTactics
 
         public void InitializeGame()
         {
-            int y = 0;
-            int x = 0;
-            for (int i = 0; i < 200; i++)
+            int g_y = 0;
+            int g_x = 0;
+            int w_y = 150;
+            int w_x = 100;
+            int b_y = 150;
+            int b_x = 0;
+            for (int g = 0; g < 48; g++)
             {
-                if (i == 0)
+                if (g == 0)
                 {
                     BlockGen Grass = new BlockGen(0, 0);
-                    map.Add(Grass);
+                    Walkablemap.Add(Grass);
                 }
-                else if (i%20 == 0)
+                else if (g % 16 == 0)
                 {
-                    x = 0;
-                    y  += 50;
-                    BlockGen Grass = new BlockGen(x, y);
-                    map.Add(Grass);
+                    g_x = 0;
+                    g_y += 50;
+                    BlockGen Grass = new BlockGen(g_x, g_y);
+                    Walkablemap.Add(Grass);
                 }
                 else
                 {
-                    x += 50;
-                    BlockGen Grass = new BlockGen(x, y);
-                    map.Add(Grass);
+                    g_x += 50;
+                    BlockGen Grass = new BlockGen(g_x, g_y);
+                    Walkablemap.Add(Grass);
+                }
+            }
+
+            for (int w = 0; w < 28; w++)
+            {
+                if (w == 0)
+                {
+                    BlockGen Water = new BlockGen(100, 150);
+                    Seamap.Add(Water);
+                }
+                else if (w % 14 == 0)
+                {
+                    w_x = 100;
+                    w_y += 50;
+                    BlockGen Water = new BlockGen(w_x, w_y);
+                    Seamap.Add(Water);
+                }
+                else
+                {
+                    w_x += 50;
+                    BlockGen Water = new BlockGen(w_x, w_y);
+                    Seamap.Add(Water);
+                }
+            }
+
+            for (int b = 0; b < 4; b++)
+            {
+                if (b == 0)
+                {
+                    BlockGen Bridge = new BlockGen(0, 150);
+                    Bridgemap.Add(Bridge);
+                }
+                else if (b%2 == 0)
+                {
+                    b_x = 0;
+                    b_y += 50;
+                    BlockGen Bridge = new BlockGen(b_x, b_y);
+                    Bridgemap.Add(Bridge);
+                }
+                else
+                {
+                    b_x += 50;
+                    BlockGen Bridge = new BlockGen(b_x, b_y);
+                    Bridgemap.Add(Bridge);
                 }
             }
         }
 
         private void InitiationLevel_Paint(object sender, PaintEventArgs e)
         {
-            foreach (BlockGen Grass in map)
+            foreach (BlockGen Grass in Walkablemap)
             {
                 e.Graphics.FillRectangle(greenBrush, Grass.x, Grass.y, Grass.size, Grass.size);
+            }
+
+            foreach (BlockGen Water in Seamap)
+            {
+                e.Graphics.FillRectangle(blueBrush, Water.x, Water.y, Water.size, Water.size);
+            }
+
+            foreach (BlockGen Bridge in Bridgemap)
+            {
+                e.Graphics.FillRectangle(blackBrush, Bridge.x, Bridge.y, Bridge.size, Bridge.size);
             }
         }
     }
